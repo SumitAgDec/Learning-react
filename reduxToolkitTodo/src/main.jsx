e5ae8a23f83744f43239b438a05ea904046de4b2
sumit <sumitqwer1234@gmail.com>
2024-10-26 23:23:29 +0530

    redux toolkit and todo

--- reduxToolkitTodo/src/main.jsx
+++ reduxToolkitTodo/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
+import AddTodo from './components/AddTodo.jsx'
+import Todos from './components/Todos.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    <AddTodo />
+    <Todos />
+  </Provider>,
 )
