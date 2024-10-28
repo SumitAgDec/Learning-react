21c91eec4ac77dd50ee22d852d3bf242b5d40364
sumit <sumitqwer1234@gmail.com>
2024-10-28 13:06:28 +0530

    create store and slices

--- 12megablog/src/App.jsx
+++ 12megablog/src/App.jsx
@@ -1,15 +1,35 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import { login, logOut } from './store/authSlice'
+import { Header, Footer } from './components'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logOut())
+        }
+      })
+      .finally(()=> setLoading(false))
+  }, [])
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        Todo: {/* <Outlet /> */}
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
